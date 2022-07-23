a06f8e4f3146256e7a093ef9eb8ef883c8d49e23
Shungo Nagamachi <shungo.nagamachi@vacancorp.com>
2022-07-24 00:39:34 +0900

    remove dist from git

--- dist/domain/model/hand.value.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Hand = void 0;
-exports.Hand = {
-    ROCK: "rock",
-    PAPER: "paper",
-    SCISSORS: "scissors",
-};
-//# sourceMappingURL=hand.value.js.map
