61701ece0390e1b1e9c7ff2cd21f1a3c5730e883
Shungo Nagamachi <shungo.nagamachi@vacancorp.com>
2022-07-23 16:51:56 +0900

    add: express.static

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
