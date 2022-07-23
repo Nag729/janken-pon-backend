7423264106fa174ad8be5b00dab8594d95765398
Shungo Nagamachi <shungo.nagamachi@vacancorp.com>
2022-07-23 17:47:22 +0900

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: `http://localhost:3000`,
+        origin: `*`,
         methods: [`GET`, `POST`],
     },
 });
